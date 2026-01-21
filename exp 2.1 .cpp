e2afe84664721a58dd4c824044dccb63bd643e25
Apeksha30121995 <apjagtap2013@gmail.com>
2026-01-21 10:57:10 +0530

    Add files via upload

--- exp 2.1 .cpp
+++ exp 2.1 .cpp
@@ -7,6 +7,8 @@ int a[2][2];
 int b[2][2];
 int c[2][2];
 int i, j;
+int k;
+
 do
 {
 cout<<"1 : Create 2 matrix"<<endl;
@@ -15,6 +17,7 @@ cout<<"3 : Addition"<<endl;
 cout<<"4 : Subtraction"<<endl;
 cout<<"5 : Multiplication"<<endl;
 cout<<"6 : Transpose"<<endl;
+cout<<"7 : Inverse of matrix"<<endl;
 int ch;
 cout<<"Enter your choice "<<endl;
 cin>>ch;
@@ -93,9 +96,12 @@ for(i=0; i<2; i++)
 {
     for (j=0; j<2; j++)
     {
-     
- c[i][j]=a[i][j]*b[i][j];
- cout<<c[i][j]<<" ";
+        c[i][j]=0;
+        for(k=0; k<2; k++)
+        {
+            c[i][j]=a[i][k]*b[k][j];
+            cout<<c[i][j]<<" ";
+        }
 }
 cout<<endl;
 }
@@ -107,12 +113,38 @@ for(i=0; i<2; i++)
 {
     for(j=0; j<2; j++)
     {
-    c[i][j]=a[j][i];
-    cout<<a[i][j]<< "  ";
+    c[j][i]=a[i][j];
+    cout<<c[j][i]<< "  ";
     }
     cout<<endl; 
 }
 break ;
+case 7:
+        {
+            float inv[2][2];
+            int det = a[0][0]*a[1][1] - a[0][1]*a[1][0];
+
+            if(det == 0)
+            {
+                cout<<"Inverse does not exist (Determinant = 0)"<<endl;
+            }
+            else
+            {
+                cout<<"Inverse of matrix A:"<<endl;
+                inv[0][0] =  a[1][1] / (float)det;
+                inv[0][1] = -a[0][1] / (float)det;
+                inv[1][0] = -a[1][0] / (float)det;
+                inv[1][1] =  a[0][0] / (float)det;
+
+                for(i=0;i<2;i++)
+                {
+                    for(j=0;j<2;j++)
+                        cout<<inv[i][j]<<" ";
+                    cout<<endl;
+                    }
+            }
+            break;
+}
 }
 
 cout<<"Do you want to continue ?"<<endl;
